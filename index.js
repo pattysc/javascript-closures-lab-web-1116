d9a3d93883d18ad2a664cfe249482c7b8d34e156
pattysc <pattys@users.noreply.github.com>
2017-01-18 14:41:40 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 const app = "I don't do much."
+
+function bumpCounter(){
+  var counter = 0
+  function addBump(){
+    counter++
+  }
+
+  function getBumps(){
+    return counter
+  }
+
+  return { getBumps, addBump }
+}
+
+function createAnimal(animalType){
+  return function (deadlyDevice){
+    return { animalType, deadlyDevice }
+  }
+}
+
+var sharkCreator = createAnimal('Shark')
+
+var sharkWithFrickinLaserbeam = sharkCreator('Laserbeam')
+var sharkWithFrickinCannon = sharkCreator('Cannon')
